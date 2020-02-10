c56872475702c871b7e367aa8879f40dd7b6c598
MAREGA OUMAR <OMarega@MacBook-Pro-de-MAREGA.local>
2020-02-10 23:31:43 +0100

    update module

--- noteModule.js
+++ noteModule.js
@@ -1,3 +1,21 @@
+$(function() {
+let token = localStorage.getItem('token')
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+
+axios.get('https://127.0.0.1:3000/token',token)
+                .then(function (response) {
+                    console.log(response)
+                })
+                .catch(function (error) {
+                    // handle error
+                    console.log(error);
+                })
+  });
+
+
+
+
+
 function getModuleRated(){
             
     let doc = document.getElementById('module').textContent;
